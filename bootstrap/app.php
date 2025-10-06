48e201ec5e6e3d41d9af6fd2790ec18adb37fb7b
Pedro Pinto <61921159+Pedro-Jorge18@users.noreply.github.com>
2025-10-06 20:48:26 +0100

    Revert "Feature login luan"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,15 +6,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Middleware\HandleCors;
-use Laravel\Sanctum\Http\Middleware\CheckAbilities;
-use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -26,27 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        $middleware->api(prepend: [
-            EnsureFrontendRequestsAreStateful::class,
-
-        ]);
-        //https://laravel.com/docs/12.x/sanctum
-        $middleware->alias([
-            'abilities' => CheckAbilities::class,
-            //vai verificar todas habilidades do token de api.
-            ////ATENÇÃO NA HORA DE FAZER AS ROTAS, PARA PROTEGER. NO FIM DA ROUTE })->middleware(['auth:sanctum', 'abilities: router');
-            'ability' => CheckForAnyAbility::class,
-            // aqui vai garantir pelo menos uma.
-            //ATENÇÃO NA HORA DE FAZER AS ROTAS, PARA PROTEGER. NO FIM DA ROUTE })->middleware(['auth:sanctum', 'ability:moderator,admin']);
-        ]);
     })
-    ->withMiddleware(function (Middleware $middleware) {
-        //Aceita a requisições do react
-        $middleware->prepend(HandleCors::class);
-    })
-
     ->withExceptions(function (Exceptions $exceptions) {
-
-
+        //
     })->create();
